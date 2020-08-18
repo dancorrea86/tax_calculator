6e18c97d175598f3c42cec49444b672de974692a
dancorrea86 <danielmacielcorrea@gmail.com>
2020-08-17 22:33:37 -0300

    commit

--- backend/index.py
+++ backend/index.py
@@ -17,8 +17,28 @@ while True:
         continue
     break
 
+def validation_if_input_isNumber():
+    
+def valation_input_issqn_rate():
+    while True:
+        print('ola')
+        issqn_rate = input()
+
+        if (issqn_rate < 0.02 and issqn_rate > 0.05):
+            print("Por favor, a alíquota deve ser maior do que '2%' e menor que '5%'")
+            continue
+
+        try:
+            issqn_rate = float(issqn_rate)
+        except:
+            print('Por favor, informe um número.')
+            continue
+        break
+    return issqn_rate
+
 # working in a new branch
-issqn_rate = float(input('Qual a alíquota do ISSQN: '))
+print('Informe a alíquota de ISSQN')
+issqn_rate = valation_input_issqn_rate()
 issqn_retention_of_moth = input('Qual o valor da retenção: ')
 pis_retention_of_moth = input('Qual o valor da retenção: ')
 cofins_retention_of_moth = input('Qual o valor da retenção: ')
